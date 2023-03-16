afa867c1692820876581c5163bad861d35ffe8d4
jomulder <jomulder@gmail.com>
2023-03-16 13:12:05 +0100

    update readme

--- R/shrinkem_main_functions.R
+++ R/shrinkem_main_functions.R
@@ -30,16 +30,15 @@
 #' # EXAMPLE
 #' estimates <- -5:5
 #' covmatrix <- diag(11)
-#' # Bayesian horseshoe where all beta's have the same global shrinkage
-#' # parameter (using 'group' argument)
-#' shrink1 <- shrinkem(estimates, covmatrix, type="horseshoe", group=rep(1,11))
+#' # Bayesian horseshoe where all beta's have the same global shrinkage (using default 'group' argument)
+#' shrink1 <- shrinkem(estimates, covmatrix, type="horseshoe")
 #' # posterior modes of middle three estimates are practically zero
 #' print(shrink1)
 #' # how traceplots
 #' bayesplot::mcmc_trace(shrink1$draws$beta)
 #' # plot posterior densities
 #' bayesplot::mcmc_areas_ridges(shrink1$draws$beta)
-#' # Bayesian horseshoe where first two and last two beta's have different global shrinkage parameter
+#' # Bayesian horseshoe where first three and last three beta's have different global shrinkage parameter
 #' # than other beta's
 #' shrink2 <- shrinkem(estimates, covmatrix, type="horseshoe", group=c(rep(1,3),rep(2,5),rep(1,3)))
 #' # posterior modes of middle five estimates are practically zero
